bc78a50460d88305cbda8635294a2140383d958e
AJ <info@adaptedpro.net>
2015-01-20 17:22:49 -0800

    Adding home module

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext, loader
+from django.utils import timezone
+from django.views import generic
+    
+class IndexView(generic.ListView):
+    template_name = 'home/index.html'
+    
+    def get_queryset(self):
+        print 'hello'
