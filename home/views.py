f1aa8463c5121341d57c0e684a4aa0bb33530662
AJ <info@adaptedpro.net>
2015-01-21 17:30:23 -0800

    Adding upload functionality.

--- home/views.py
+++ home/views.py
@@ -1,12 +1,27 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
-from django.utils import timezone
-from django.views import generic
-    
-class IndexView(generic.ListView):
-    template_name = 'home/index.html'
-    
-    def get_queryset(self):
-        print 'hello'
+from home.forms import UploadFileForm
+
+def index(request):
+    template = loader.get_template('home/index.html')
+    message = 'Convert Your Image to ASCII'
+    form_message = ''
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #form.save()
+            form_message += 'so good'
+#     else:
+#         form = UploadFileForm()
+#     return render_to_response('home/uploaded.html', {'form': form})    
+
+    context = RequestContext(request, {
+        'title':'What Fun',
+        'message': message,
+        'upload_message' : request.method,
+        'form_message' : form_message
+    })
+    return HttpResponse(template.render(context)) 
